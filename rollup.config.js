45423d761d834f13df861b3bd38867bc1bdd9cac
Taras Mankovski <tarasm@gmail.com>
2018-04-03 19:13:00 -0400

    All of the tests are passing

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,44 @@
-
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import pkg from './package.json'
+const babel = require("rollup-plugin-babel");
+const resolve = require("rollup-plugin-node-resolve");
+const pkg = require("./package.json");
+const commonjs = require("rollup-plugin-commonjs");
 
 const globals = {
-  'lodash.curry': '_.curry',
-  'object.getownpropertydescriptors': 'Object.getOwnPropertyDescriptors'
+  "lodash.curry": "_.curry",
+  "object.getownpropertydescriptors": "Object.getOwnPropertyDescriptors"
 };
 
 let external = Object.keys(globals);
 
-export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/funcadelic.js',
-    output: {
-      name: 'funcadelic',
+module.exports = {
+  input: "src/funcadelic.js",
+  output: [
+    {
+      name: "funcadelic",
       file: pkg.browser,
       globals,
-      format: 'umd'
+      format: "umd"
     },
-    external,
-    plugins: [            
-      babel({
-        runtimeHelpers: true,
-        babelrc: false,
-        comments: false,
-        presets: [
-          [
-            "env",
-            {
-              modules: false
-            }
-          ]
-        ],
-        plugins: ["external-helpers"]
-      }),
-      resolve(), 
-      commonjs()
-    ]
-  },
-  {
-    input: 'src/funcadelic.js',
-    external,
-    output: [
-			{ file: pkg.main, format: 'cjs' }, 
-			{ file: pkg.module, format: 'es' }
-		]
-  }
-]
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" }
+  ],
+  external,
+  plugins: [
+    babel({
+      runtimeHelpers: true,
+      babelrc: false,
+      comments: false,
+      presets: [
+        [
+          "env",
+          {
+            modules: false
+          }
+        ]
+      ],
+      plugins: ["external-helpers"]
+    }),
+    resolve(),
+    commonjs()
+  ]
+};
